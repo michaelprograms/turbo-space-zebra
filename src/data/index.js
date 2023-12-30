acecc7154eadd62907c5e95bbf6b107b7719869d
Michael Programs <michael.programs@gmail.com>
2023-12-30 03:09:02 +0000

    update dexie definition to only index id and name

--- src/data/index.js
+++ src/data/index.js
@@ -2,5 +2,5 @@ import Dexie from 'dexie';
 
 export const db = new Dexie('userdata');
 db.version(1).stores({
-  maps: '++id, name, width, height, created, edited, data',
+  maps: '++id, name',
 });
