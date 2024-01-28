32e4cc7cd6903758f9f915a4bb85fe6335dbd258
Michael Programs <michael.programs@gmail.com>
2024-01-27 20:37:35 -0500

    fix map-grid-room exit link diagonal alignment

--- src/components/map-grid-room/style.js
+++ src/components/map-grid-room/style.js
@@ -27,7 +27,7 @@ export const MapGridRoomWrapper = styled.div`
     height: 50%;
     overflow: hidden;
     position: absolute;
-    bottom: 0;
+    bottom: 0px;
   }
   .line-southeast-wrapper {
     width: 50%;
@@ -35,17 +35,19 @@ export const MapGridRoomWrapper = styled.div`
     overflow: hidden;
     position: absolute;
     right: 0;
-    bottom: 0;
+    bottom: 0px;
   }
   .line-northwest {
     width: 142%;
     height: 4px;
+    top: -3px;
     transform-origin: top left;
     transform: rotate(45deg);
   }
   .line-northeast {
     width: 142%;
     height: 4px;
+    top: -3px;
     right: 0;
     transform-origin: top right;
     transform: rotate(-45deg);
@@ -53,14 +55,14 @@ export const MapGridRoomWrapper = styled.div`
   .line-southwest {
     width: 142%;
     height: 4px;
-    bottom: 0;
+    bottom: -3px;
     transform-origin: bottom left;
     transform: rotate(-45deg);
   }
   .line-southeast {
     width: 142%;
     height: 4px;
-    bottom: 0;
+    bottom: -3px;
     right: 0;
     transform-origin: bottom right;
     transform: rotate(45deg);
@@ -104,7 +106,7 @@ export const RoomCircle = styled.div`
   width: 19px;
   height: 19px;
   box-sizing: border-box;
-  position: relative;
+  position: absolute;
   left: 50%;
   top: 50%;
   transform: translateX(-50%) translateY(-50%);
